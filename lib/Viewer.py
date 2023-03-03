adcca8b1c700f1515f1e82ffac74d61331b5d5b6
Madison Evans <m.corbinevans@gmail.com>
2023-03-03 07:59:35 -0700

    2 tests passed

--- lib/Viewer.py
+++ lib/Viewer.py
@@ -3,17 +3,17 @@ class Viewer:
     def __init__(self, username):
         if len(username) <= 6 or len(username) >= 16: 
             raise Exception("Please enter a username that is proper length")
-        self.username = username
+        self._username = username
 
     @property
     def username(self): 
-        return self.username
+        return self._username
 
     @username.setter
     def username(self, username):
         if len(username) <= 6 or len(username) >= 16: 
             raise Exception("Please enter a username that is proper length")
-        self.username = username
+        self._username = username
         
     def reviewed_movie(self, movie):
         pass
